085fcc06d891fcfc82b97efad8d76baa25cf6df1
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-05-18 11:25:35 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 from config import Config
 
 class Translation(object):
-      START = str(Config.START) + "\n\nFor source code [press me](https://github.com/Ns-AnoNymouS/feedbackbot)"
+      START = str(Config.START) + "\n\nFor MOVIE GROUP [press me](https://t.me/cinemapedika)"
 
       RULES = Config.RULES
 
@@ -10,17 +10,17 @@ class Translation(object):
 
       ABOUT = """**MY DETAILS:**
 
-```🤖My Name:``` [Feedback Ns Bot](https://t.me/Feedback_Nsbot)
+```🤖My Name:``` [Feedback CP Bot](https://t.me/CPAssistbot)
     
 ```📝 Language:``` [Python 3](https://www.python.org/)
 
 ```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-```👨🏻‍💻 Developer:``` [𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬](https://t.me/Ns_AnoNymouS)
+```👨🏻‍💻 Developer:``` [carl max](https://t.me/carlmax_here)
                
-```📢 Channel:``` [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+```📢 Channel:``` [MAIN CHANNEL](https://t.me/CP_S4U_Links)
 
-```👥 Group:``` [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+```👥 Group:``` [MOVIE GROUP](https://t.me/cinemapedika)
 
 Inspired By @stemlime_bot
 """
